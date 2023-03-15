a51ae9a9a0526a8f7c76948c16a01715c7c39555
Lilit Nersisyan <txgt.lilit.nersisyan@gmail.com>
2023-03-15 20:07:02 +0400

    Delete myspecies_2022.sh

--- graphlan/myspecies_2022.sh
+++
@@ -1,9 +0,0 @@
-export PATH=`pwd`/../graphlan/:$PATH
-
-species=data/my.species_2022.txt
-annot_f=annot/myannotR_2022.txt
-out_prefix=species_2022
-
-graphlan_annotate.py --annot $annot_f $species xml/$out_prefix.xml
-graphlan.py xml/$out_prefix.xml img/$out_prefix.png --dpi 300 --size 3.5
-graphlan.py xml/$out_prefix.xml img/$out_prefix.svg --dpi 300 --size 3.5
